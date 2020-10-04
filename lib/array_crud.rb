bc8a5d6b238b012ab81ec3eb33d0d62f024605d2
madiwa Simon <mad.simon01@gmail.com>
2020-10-04 12:31:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+["w", "x", "y", "z"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["x", "y"] 
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
